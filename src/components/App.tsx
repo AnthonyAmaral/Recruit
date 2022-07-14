171021da32abfe469fe46123384fdcbb576c189a
Anthony Amaral <aamaral@hbs.edu>
2022-07-13 23:03:18 -0400

    started auth0

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,27 @@
 import { lazy, Suspense } from "react";
 import "src/styles/App.css";
 
+
 const Card = lazy(() => import("./Card"));
+const Login = lazy(() => import("./Login"));
 
 export interface AppProps {
   url: string;
 }
 
 export function App({ url }: AppProps) {
+    
   return (
     <Suspense>
       <div className="App">
-        <h1>Vite + React = {url}</h1>
+        <h1>Vite + React = {}</h1>
 
         <Card />
 
+        <Login/>
+
         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
       </div>
     </Suspense>
   );
--- src/render/render.client.tsx
+++ src/render/render.client.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { App, AppProps } from "src/components/App";
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import { getConfig } from "../config"
+
+
 import "src/styles/index.css";
 
 declare global {
@@ -10,9 +15,23 @@ declare global {
   }
 }
 
+
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin
+};
+
 hydrateRoot(
   document.getElementById("app")!,
   <StrictMode>
-    <App {...window.APP_PROPS} />
+    <Auth0Provider {...providerConfig}> 
+        <App {...window.APP_PROPS} />
+    </Auth0Provider>
+   
   </StrictMode>
 );
